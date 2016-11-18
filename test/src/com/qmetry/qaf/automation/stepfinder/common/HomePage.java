e7b77a461e0a27091c0b289978e54c9244032ec9
cjayswal <cjayswal@gmail.com>
2016-11-18 13:55:06 +0530

    Revert "Adding Step finder related tests"

--- test/src/com/qmetry/qaf/automation/stepfinder/common/HomePage.java
+++
@@ -1,9 +0,0 @@
-package com.qmetry.qaf.automation.stepfinder.common;
-
-import com.qmetry.qaf.automation.step.QAFTestStep;
-
-public interface HomePage {
-
-	@QAFTestStep(description = "open application")
-	String openApp();
-}
